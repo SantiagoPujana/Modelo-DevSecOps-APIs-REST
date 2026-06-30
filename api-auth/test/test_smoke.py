ff5f2db2363da8468fa72b710987e834d550211b
Santiago Pujana <santiagopujana@gmail.com>
2026-06-30 15:33:43 -0500

    Add reusable DevSecOps pipeline phases and vulnerable app containerization25

--- api-auth/test/test_smoke.py
+++
@@ -1,8 +0,0 @@
-from fastapi.testclient import TestClient
-import main
-
-client = TestClient(main.app)
-
-def test_smoke():
-    r = client.get("/hash", params={"password": "x"})
-    assert r.status_code == 200
